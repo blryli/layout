b8835dd4cb95a203b41a91464c8638f34e5f1eb3
blryli <405549416@qq.com>
2025-02-20 15:37:09 +0800

    光影区设计

--- utils.js
+++ utils.js
@@ -1,4 +1,38 @@
-function add(d, i) {
+function draw(group, index) {
+  const attrs = {
+    selectable: !group.some(d => d.disabled),
+    scalable: false,
+    rotatable: false,
+  }
+  const rect = new fabric.Group(group, attrs)
+  rect.zIndex = index
+  canvas.add(rect)
+  // canvas.setActiveObject(rect)
+  // 禁用矩形的默认旋转行为
+  // rect.lockRotation = true;
+  // rect.lockMovementY = true;
+  // rect.scalable = false;
+
+  // 监听矩形的旋转事件（注意：Fabric.js没有直接的'rotating'事件，所以我们使用'modified'事件）
+  // rect.on('modified', function() {
+  //   // 获取当前角度
+  //   let angle = rect.getAngle();
+  //   console.log('angle',angle)
+    
+  //   // 计算最接近的90度或180度
+  //   let newAngle = Math.round(angle / 90) * 90;
+    
+  //   // 如果新角度与当前角度不同，则更新角度
+  //   if (newAngle !== angle) {
+  //     rect.setAngle(newAngle);
+  //     // 由于我们禁用了默认旋转行为，所以需要手动设置坐标和渲染
+  //     rect.setCoords();
+  //     canvas.renderAll();
+  //   }
+  // });
+}
+
+function addOne(d, i) {
   // 图形
   const rect = new fabric[d.type || 'Rect']({
     ...{
@@ -22,52 +56,27 @@ function add(d, i) {
     originY: "center",
   });
 
-  // 有分组添加进分组
   const {groupName} = d
-  if(groupName) {
-    if(!groups[groupName]) groups[groupName] = []
-    groups[groupName].push(rect, text)
-  } else { // 无分组直接绘制
-    draw([rect, text])
-  }
+  const name = groupName || `item${i}`
+  if(!groups[name]) groups[name] = []
+  groups[name].push(rect, text)
   
   // 绘制分组
   if(i === renders.length - 1) {
-    Object.values(groups).forEach(g => {
-      draw(g)
+    Object.values(groups).forEach((g, i) => {
+      draw(g, i)
     })
   }
 }
 
-function draw(group) {
-  const attrs = {
-    selectable: !group.some(d => d.disabled),
-    scalable: false,
-    rotatable: false,
-  }
-  const rect = new fabric.Group(group, attrs)
-  canvas.add(rect)
-  canvas.setActiveObject(rect)
-  // 禁用矩形的默认旋转行为
-  // rect.lockRotation = true;
-  // rect.lockMovementY = true;
-  // rect.scalable = false;
+function add(params) {
+  const renders = Array.isArray(params) ? params : [params]
+  renders.forEach((d, i) => {
+    addOne(d, i)
+  })
+  // 按 zIndex 排序对象数组
+  canvas.getObjects().sort((a, b) => a.zIndex - b.zIndex)
 
-  // 监听矩形的旋转事件（注意：Fabric.js没有直接的'rotating'事件，所以我们使用'modified'事件）
-  // rect.on('modified', function() {
-  //   // 获取当前角度
-  //   let angle = rect.getAngle();
-  //   console.log('angle',angle)
-    
-  //   // 计算最接近的90度或180度
-  //   let newAngle = Math.round(angle / 90) * 90;
-    
-  //   // 如果新角度与当前角度不同，则更新角度
-  //   if (newAngle !== angle) {
-  //     rect.setAngle(newAngle);
-  //     // 由于我们禁用了默认旋转行为，所以需要手动设置坐标和渲染
-  //     rect.setCoords();
-  //     canvas.renderAll();
-  //   }
-  // });
+  // 刷新画布显示
+  canvas.requestRenderAll()
 }
